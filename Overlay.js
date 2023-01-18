20c11ccfaf138f4b68fca45ef8e7a47f2bdcbffe
TrickFire <36343043+nickolajr@users.noreply.github.com>
2023-01-18 11:15:44 +0100

    done

--- Overlay.js
+++ Overlay.js
@@ -1,12 +1,15 @@
-let openOverlayBtn = document.getElementById("openCV");
-let overlay = document.getElementById("Cv");
 
-openOverlayBtn.addEventListener("click", function() {
-  overlay.style.display = "block";
-});
-let closeOverlayBtn = document.getElementById("closeCV");
-
-closeOverlayBtn.addEventListener("click", function() {
-  overlay.style.display = "none";
-});
-
+window.addEventListener('DOMContentLoaded', function() {
+    let openOverlayBtn = document.getElementById("openCV");
+  let overlay = document.getElementById("Cv");
+  
+  openOverlayBtn.addEventListener("click", function() {
+    overlay.style.display = "block";
+  });
+  let closeOverlayBtn = document.getElementById("closeCV");
+  
+  closeOverlayBtn.addEventListener("click", function() {
+    overlay.style.display = "none";
+  });
+   });
+  
